3da96cb93692cb5f0bf5e40cfa834e5d5bee18d8
Đừng xóa <nomnom1204@gmail.com>
2018-06-19 15:41:41 +0700

    ngắn gọn hơn

--- NewScrollView/ViewController.swift
+++ NewScrollView/ViewController.swift
@@ -8,13 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIScrollViewDelegate {
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        scrollView.minimumZoomScale = 1.0
+        scrollView.maximumZoomScale = 5.0
     }
 
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
